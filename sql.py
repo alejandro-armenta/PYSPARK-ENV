3395d9f483f90091442480dcf9c6d42caf1282c0
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-12 23:31:25 -0600

    duration seconds

--- sql.py
+++ sql.py
@@ -17,11 +17,54 @@ column_split = (
         np.array(logs.columns), len(logs.columns) // 3
     )
 )
-
+"""
 for x in column_split:
     logs.select(*x).show(truncate=False)
+logs.printSchema()
+"""
 
-    
+
+logs = logs.drop("BroadcastLogID","SequenceNO")   
+
+"""
+print("BroadcastLogID" in logs.columns)
+print("SequenceNO" in logs.columns)
+
+(
+
+    logs.
+    select(
+        "Duration", 
+        F.col("Duration").substr(1,2).cast("int").alias("dur_hours"),
+        F.col("Duration").substr(4,2).cast("int").alias("dur_minutes"),
+        F.col("Duration").substr(7,2).cast("int").alias("dur_seconds"),
+    ).
+    distinct().
+    show()
+
+)
+"""
+
+(
+
+    logs.
+    select(
+        "Duration",
+        (
+            F.col("Duration").substr(1,2).cast("int") * 60 * 60 +
+            F.col("Duration").substr(4,2).cast("int") * 60 + 
+            F.col("Duration").substr(7,2).cast("int")
+        ).
+        alias("Duration_seconds") 
+    ).
+    distinct().
+    show()
+
+)
+
+
+
+
 
 """
 logs.printSchema()
